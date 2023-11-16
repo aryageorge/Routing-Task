6704ff9bc7a70385fb13a4db14b788052490314c
aryageorge <aryageorge4@gmail.com>
2023-11-16 16:58:26 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  {
+ 
   title = 'PROJECT-1';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        component:HomeComponent,
+        path:''
+    },
+    {
+        component:AboutComponent,
+        path:'About'
+    },
+    {
+        component:ContactComponent,
+        path:'Contact'
+    },
+    {
+        component:ProductsComponent,
+        path:'Product'
+    },
+
+];
